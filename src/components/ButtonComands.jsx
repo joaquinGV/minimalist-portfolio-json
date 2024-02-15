4ec7ce8413e1ea7decae95669f594b56f44a0c64
Joaquin Gonzalez <joaquinagv99@hotmail.com>
2024-02-15 13:35:12 -0600

    responsive portfolio

---
+++ src/components/ButtonComands.jsx
@@ -0,0 +1,31 @@
+const ButtonComands = () => {
+  const ninja = document.querySelector("ninja-keys");
+
+  const handleClick = () => {
+    ninja.open();
+    ninja.hotKeysJoinedView = false;
+
+    if (!ninja.querySelector('div[slot="footer"]')) {
+      // create div, set attribute slot with value "footer" and inyect as children to ninja
+      const slot = document.createElement("div");
+      slot.setAttribute("slot", "footer");
+      ninja.appendChild(slot);
+
+      const actions = ninja.shadowRoot.querySelectorAll("ninja-action");
+      actions.forEach((element) => {
+        const kbd = element.shadowRoot.querySelector("kbd");
+        kbd.textContent = "";
+      });
+    }
+  };
+  return (
+    <button
+      onClick={handleClick}
+      class="fixed bottom-4 right-4 sm:hidden bg-gray-200 m-auto text-gray-500 text-2xl hover:text-gray-900 border border-gray-300 rounded-xl px-4 py-2   items-center align-middle fill-none"
+    >
+      ⌘
+    </button>
+  );
+};
+
+export default ButtonComands;
