26daaa888e8ae73cf52593de6c7ed8b0aa2cc112
Joaquin Gonzalez <joaquinagv99@hotmail.com>
2024-02-15 02:51:06 -0600

    last

--- src/components/toggleMode.js
+++ src/components/toggleMode.js
@@ -1,21 +1,21 @@
-import { useState } from "preact/hooks";
-const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-console.log(darkModeMediaQuery.matches);
+// import { useState } from "preact/hooks";
+// const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+// console.log(darkModeMediaQuery.matches);
 
-export function toggleMode() {
-  const [isDarkMode, setIsDarkMode] = useState(darkModeMediaQuery.matches); // Obtener el estado de darkMode
+// export function toggleMode() {
+//   const [isDarkMode, setIsDarkMode] = useState(darkModeMediaQuery.matches); // Obtener el estado de darkMode
 
-  // Función para cambiar el estado de darkMode
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    if (!isDarkMode) {
-      document.querySelector("ninja-keys")?.classList.remove("dark");
-      document.documentElement.classList.remove("dark");
-    } else {
-      document.querySelector("ninja-keys")?.classList.add("dark");
-      document.documentElement.classList.add("dark");
-    }
-  };
+//   // Función para cambiar el estado de darkMode
+//   const toggleDarkMode = () => {
+//     setIsDarkMode(!isDarkMode);
+//     if (!isDarkMode) {
+//       document.querySelector("ninja-keys")?.classList.remove("dark");
+//       document.documentElement.classList.remove("dark");
+//     } else {
+//       document.querySelector("ninja-keys")?.classList.add("dark");
+//       document.documentElement.classList.add("dark");
+//     }
+//   };
 
-  return { isDarkMode, toggleDarkMode };
-}
+//   return { isDarkMode, toggleDarkMode };
+// }
